aa6d7f69e14e5108534a39953f3f0c393d564e88
Ethan <ethan.steip@gmail.com>
2025-03-10 19:25:17 -0400

    adding dependencies property to package.json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,7 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<Button>Click me</Button>
-<h1>Welcome to svelteforge</h1>
+<section class="flex h-screen w-full items-center justify-center">
+	<Button>Click me</Button>
+	<h1>Welcome to svelteforge</h1>
+</section>
